8ded2e455385d1fc5a7acf004aa3939b669991a4
msousa <moka.sousa@hotmail.com>
2026-02-27 08:46:20 -0300

    fix card

--- src/app/list-page/card/card.ts
+++ src/app/list-page/card/card.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { Component, computed, inject, input, Signal } from '@angular/core';
 
 import { CoinMarket } from '../../../client';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.html',
   styleUrl: './card.scss',
 })
@@ -13,4 +14,5 @@ export class Card {
   private sanitizer = inject(DomSanitizer);
   public readonly coin: Signal<CoinMarket> = input.required<CoinMarket>();
   protected readonly image: Signal<SafeUrl> = computed(() => this.coin().image && this.sanitizer.bypassSecurityTrustUrl(this.coin().image));
+  // this.sanitizer.sanitize(SecurityContext.URL, url)
 }
--- src/client/models/coin-market.ts
+++ src/client/models/coin-market.ts
@@ -4,6 +4,7 @@ export class CoinMarket {
   public name: string;
   public image: string;
   public down: boolean;
+  public symbol: string;
   public currentPrice: number;
   public priceChange24h: number;
 
@@ -11,6 +12,7 @@ export class CoinMarket {
     this.id = data?.id;
     this.name = data?.name;
     this.image = data?.image;
+    this.symbol = data?.symbol;
     this.currentPrice = data?.current_price || 0;
     this.priceChange24h = data?.price_change_percentage_24h || 0;
     this.up = this.priceChange24h > 0;
